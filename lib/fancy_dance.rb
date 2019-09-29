d86615ab799a5b3328f3ffca3b6a5033769cd8cf
gregarious-number-3828 <muse.malize@gmail.com>
2019-09-29 20:50:36 +0000

    Done.

---
+++ lib/fancy_dance.rb
@@ -0,0 +1,32 @@
+module FancyDance
+  
+  # Instance methods. Includes.
+  module InstanceMethods
+ 
+    def twirl
+      "I'm twirling!"
+    end
+ 
+    def jump
+      "Look how high I'm jumping!"
+    end
+ 
+    def pirouette
+      "I'm doing a pirouette"
+    end
+ 
+    def take_a_bow
+      "Thank you, thank you. It was a pleasure to dance for you all."
+    end
+  end
+  
+  # Class methods. Extends.
+  module ClassMethods
+ 
+    def metadata
+      "This class produces objects that love to dance."
+    end
+    
+  end
+  
+end
